bfcba7a36294c27c8d469dd82c4be4adf1eddfca
Denis Glazkov <glazzk.off@mail.ru>
2025-09-09 02:58:14 +0300

    Format main.cpp for aurora

--- auroraApp/src/main.cpp
+++ auroraApp/src/main.cpp
@@ -7,17 +7,17 @@
 #include <QQuickView>
 
 int main(int argc, char* argv[]) {
-  qmlRegisterType<Greeting>("Shared", 1, 0, "Greeting");
-  qmlRegisterType<GithubRepository>("Shared", 1, 0, "GithubRepository");
-  qmlRegisterType<Database>("Shared", 1, 0, "Database");
+    qmlRegisterType<Greeting>("Shared", 1, 0, "Greeting");
+    qmlRegisterType<GithubRepository>("Shared", 1, 0, "GithubRepository");
+    qmlRegisterType<Database>("Shared", 1, 0, "Database");
 
-  QScopedPointer<QGuiApplication> app(Aurora::Application::application(argc, argv));
-  app->setOrganizationName("com.example");
-  app->setApplicationName("aurorakmp");
+    QScopedPointer<QGuiApplication> app(Aurora::Application::application(argc, argv));
+    app->setOrganizationName("com.example");
+    app->setApplicationName("aurorakmp");
 
-  QScopedPointer<QQuickView> view(Aurora::Application::createView());
-  view->setSource(Aurora::Application::pathTo("qml/com.example.aurorakmp.qml"));
-  view->show();
+    QScopedPointer<QQuickView> view(Aurora::Application::createView());
+    view->setSource(Aurora::Application::pathTo("qml/com.example.aurorakmp.qml"));
+    view->show();
 
-  return app->exec();
+    return app->exec();
 }
